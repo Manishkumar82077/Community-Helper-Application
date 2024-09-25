bb9efaef4c7c649740c441a0b739596f4bbf6ca5
manish <manishkumar7543811190@gmail.com>
2024-09-25 08:59:23 +0530

    mongodb connection and and the models crested

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 import  express from "express";
 import cookieParser from "cookie-parser";
 import cors from"cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 const app = express();
 
-app.get("/home",(req,res)=>{
-    return res.status(200).json({
-        message:"I am coming from backend ",
-        success:true
-    })
-})
+// app.get("/home",(req,res)=>{
+//     return res.status(200).json({
+//         message:"I am coming from backend ",
+//         success:true
+//     })
+// })
 
 
 // middleware
@@ -23,8 +26,9 @@ app.use(cors(corsOptions));
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`server running at port : ${PORT}` )
 })
 
